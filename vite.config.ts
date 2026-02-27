45e3e31d54a2a437de80fd31c2633e2eaf2fb40a
jingle chen <47862618+jinglechen2287@users.noreply.github.com>
2026-02-27 13:38:43 -0800

    bump dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import webspatial from '@webspatial/vite-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webspatial from "@webspatial/vite-plugin";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), webspatial()],
-})
+  plugins: [
+    react(),
+    webspatial(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          XR_ENV: process.env.XR_ENV,
+        },
+      },
+    }),
+  ],
+});
